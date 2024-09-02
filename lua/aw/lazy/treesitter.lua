576f66c73c60e59020e46122908f26d2a56f804e
Wingfooted <Wingfooted3@gmail.com>
2024-09-02 12:31:03 +1000

    nvimr

--- lua/aw/lazy/treesitter.lua
+++ lua/aw/lazy/treesitter.lua
@@ -3,11 +3,11 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = {"r", "lua", "python", "sql"},
+			ensure_installed = {"r", "lua", "python", "sql", "markdown", "markdown_inline", "rnoweb", "yaml"},
 			sync_install = false,
 			indent = {enable = true},
 			hilight = {
-				enable = true, 
+				enable = true,
 				additional_vim_regex_highlighting = { "markdown" }
 			},
 		})
