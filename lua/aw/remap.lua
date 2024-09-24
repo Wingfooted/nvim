08fa8c2dccfd8bf70ee67a943151b65317650dc4
Wingfooted <Wingfooted3@gmail.com>
2024-09-24 21:08:09 +1000

    adding remaps

--- lua/aw/remap.lua
+++ lua/aw/remap.lua
@@ -1,6 +1,8 @@
 
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>b", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>t", vim.cmd.tabnext)
+vim.keymap.set("n", "<leader>b", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>s", vim.cmd.split)
 vim.api.nvim_set_keymap('n', '<F6>', '<Plug>(IPy-RunAll)', { silent = true })
